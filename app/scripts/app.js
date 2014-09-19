06022535f17dc1d21be238519f4e0d79c1290581
sorashiro <yufei930101@gmail.com>
2014-09-19 15:45:34 +0800

    update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,17 +7,53 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'LocalStorageModule'
   ])
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'IndexCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/items', {
+        templateUrl: 'views/items.html',
+        controller: 'ItemsCtrl'
+      })
+      .when('/cart', {
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl'
+      })
+      .when('/pay', {
+        templateUrl: 'views/pay.html',
+        controller: 'PayCtrl'
+      })
+      .when('/categoryManager', {
+        templateUrl: 'views/categoryManager.html',
+        controller: 'CategoryCtrl'
+      })
+      .when('/goodsInformationManager', {
+        templateUrl: 'views/goodsInformationManager.html',
+        controller: 'GoodsInformationCtrl'
+      })
+      .when('/addCategory', {
+        templateUrl: 'views/addCategory.html',
+        controller: 'CategoryCtrl'
+      })
+      .when('/addGoods', {
+        templateUrl: 'views/addGoods.html',
+        controller: 'GoodsInformationCtrl'
+      })
+      .when('/modifyCategory', {
+        templateUrl: 'views/modifyCategory.html',
+        controller: 'ModifyCategoryCtrl'
+      })
+      .when('/modifyGoodsInformation', {
+        templateUrl: 'views/modifyGoodsInformation.html',
+        controller: 'ModifyGoodsCtrl'
       })
       .otherwise({
         redirectTo: '/'
