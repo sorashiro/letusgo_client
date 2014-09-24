7cf1469ad5ba5b063ab049288dce9d4bca94b5d3
sorashiro <yufei930101@gmail.com>
2014-09-24 20:46:28 +0800

    update add item function with http

--- app/scripts/service/goodsInformationService.js
+++ app/scripts/service/goodsInformationService.js
@@ -1,52 +1,74 @@
 'use strict';
 
-angular.module('letusgoApp').service('GoodsInformationService', function (localStorageService, ItemsService) {
+angular.module('letusgoApp').service('GoodsInformationService', function ($http, $location, localStorageService, ItemsService) {
 
-  this.addGoods = function (category, name, unit, price) {
-    var items = ItemsService.get('itemsList');
-    items.push({'category': category, 'name': name, 'unit': unit, 'price': price});
-    ItemsService.add('itemsList', items);
+  this.addGoods = function(category, name, unit, price) {
+    ItemsService.loadItems(function(data) {
+      var items = data;
+      var ids = _.pluck(items, 'id');
+      var maxId = _.max(ids);
+      var item = {category: category, name: name, unit: unit, price: price, id: maxId+1};
+
+      items.push(item);
+      $http.post('/api/items/', {items: items});
+    });
   };
 
   this.remove = function (goodsInformation) {
-    var items = ItemsService.get('itemsList');
-
-    for (var i = 0; i < items.length; i++) {
-      if (goodsInformation.name === items[i].name) {
-        items.splice(i, 1);
-      }
-    }
-    ItemsService.add('itemsList', items);
+    $http.delete('/api/items/' + goodsInformation.id);
   };
 
-  this.change = function (goodsInformation) {
-    var items = ItemsService.loadItems();
-    var item;
+//  this.change = function (goodsInformation) {
+//    var items = ItemsService.loadItems();
+//    var item;
+//
+//    for (var i = 0; i < items.length; i++) {
+//      if (goodsInformation.name === items[i].name) {
+//        item = items[i];
+//      }
+//      ItemsService.add('modifyItem', item);
+//    }
+//  };
+//
+  this.changeItem = function(item, callback) {
 
-    for (var i = 0; i < items.length; i++) {
-      if (goodsInformation.name === items[i].name) {
-        item = items[i];
-      }
-      ItemsService.add('modifyItem', item);
-    }
+    $http.get('/api/items/' + item.id).success(function(data) {
+      callback(data);
+    });
   };
 
-  this.getItem = function () {
-    var item = ItemsService.get('modifyItem') || [];
-    return item;
+//  this.getItem = function () {
+//    var item = ItemsService.get('modifyItem') || [];
+//    return item;
+//  };
+
+//  this.getItem = function(callback) {
+//    var item = this.change();
+//    $http.get('/api/items/' + goodsInformation.id).success(function(data) {
+//      callback(data);
+//    })
+//  };
+
+//  this.modify = function (category, name, unit, price) {
+//    var items = ItemsService.get('itemsList');
+//    var item = this.getItem();
+//    var newItem = {category: category, name: name, unit: unit, price: price};
+//
+//    for (var i = 0; i < items.length; i++) {
+//      if (item.name === items[i].name) {
+//        items.splice(i, 1, newItem);
+//        break;
+//      }
+//    }
+//    ItemsService.add('itemsList', items);
+//  };
+//});
+
+  this.modify = function (item, category, name, unit, price) {
+
+    var newItem = {category: category, name: name, unit: unit, price: price, id: item.id};
+
+    $http.put('/api/items/' + item.id, {item: newItem});
   };
 
-  this.modify = function (category, name, unit, price) {
-    var items = ItemsService.get('itemsList');
-    var item = this.getItem();
-    var newItem = {'category': category, 'name': name, 'unit': unit, 'price': price};
-
-    for (var i = 0; i < items.length; i++) {
-      if (item.name === items[i].name) {
-        items.splice(i, 1, newItem);
-        break;
-      }
-    }
-    ItemsService.add('itemsList', items);
-  };
 });
