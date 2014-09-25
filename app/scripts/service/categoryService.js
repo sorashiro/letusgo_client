544c53afab9da6e745fa0b6b08b5f492e201f7bd
sorashiro <yufei930101@gmail.com>
2014-09-25 14:29:58 +0800

    delete unuseful codes

--- app/scripts/service/categoryService.js
+++ app/scripts/service/categoryService.js
@@ -20,57 +20,7 @@ angular.module('letusgoApp').service('CategoryService', function ($http, localSt
     })
   };
 
-//  this.removes = function (category) {
-//    var items = ItemsService.get('itemsList') || [];
-//
-//    for (var i = 0; i < items.length; i++) {
-//      if (category === items[i].category) {
-//        if (items[i].name) {
-//          alert('该分类下有商品，不能删除！');
-//          break;
-//        }
-//        else {
-//          items.splice(i, 1);
-//          i--;
-//        }
-//      }
-//    }
-//    ItemsService.add('itemsList', items);
-//  };
-
-  function existItem(category, callback) {
-    var items = ItemsService.loadItems();
-
-      _.forEach(items, function (item) {
-        if (category.category === item.category) {
-          alert('该分类下有商品，不能删除~');
-          return false;
-        }
-        else{
-          callback(true);
-        }
-      });
-    }
-
-//  this.removes = function (category) {
-//
-//    ItemsService.loadItems(function (data) {
-//      var items = data;
-//
-//      _.forEach(items, function(item) {
-//        if(category.category === item.category) {
-//          alert('该分类下有商品，不能删除~');
-//          return false;
-//        }
-//        else {
-//          $http.delete('/api/categories/' + category.id);
-//        }
-//      })
-//    });
-//  };
-
   this.removes = function(category) {
-
       $http.delete('/api/categories/' + category.id);
   };
 
